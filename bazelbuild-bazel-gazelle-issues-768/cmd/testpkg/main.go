e8b4f6b8787641ed0122162e6d668016e41a038a
Steven Wijaya <stevenwjy149@gmail.com>
2020-04-29 02:47:53 +0800

    Add bazelbuild-bazel-gazelle-issues-768

---
+++ bazelbuild-bazel-gazelle-issues-768/cmd/testpkg/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+  "context"
+  "log"
+  "os"
+  "time"
+
+  "github.com/stevenwjy/private-repo-test/protos"
+  "google.golang.org/grpc"
+)
+
+const (
+  address     = "localhost:8081"
+)
+
+func main() {
+  // Set up a connection to the server.
+  conn, err := grpc.Dial(address, grpc.WithInsecure())
+  if err != nil {
+    log.Fatalf("did not connect: %v", err)
+  }
+  defer conn.Close()
+  c := protos.NewTestServiceClient(conn)
+
+  // Contact the server and print out its response.
+  ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+  defer cancel()
+  r, err := c.FooQuery(ctx, &protos.FooRequest{a: "a", b: "b"})
+  if err != nil {
+    log.Fatalf("could not query: %v", err)
+  }
+  log.Printf("Response: %s", r.msg)
+}
---
+++ bazelbuild-bazel-gazelle-issues-768/go.mod
@@ -0,0 +1,8 @@
+module github.com/stevenwjy/bugtest
+
+go 1.13
+
+require (
+  google.golang.org/grpc v1.29.1
+  github.com/stevenwjy/private-repo-test latest
+)
